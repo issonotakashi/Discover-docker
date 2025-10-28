40b053ab477761579d0ea5e762d9dd350345398f
issonotakashi <sami.semlali@efrei.net>
2025-10-28 16:41:21 +0100

    API: add GET /departments to support frontend list

--- backend-spring/simpleapi/src/main/java/fr/takima/training/simpleapi/controller/DepartmentController.java
+++ backend-spring/simpleapi/src/main/java/fr/takima/training/simpleapi/controller/DepartmentController.java
@@ -1,6 +1,8 @@
 package fr.takima.training.simpleapi.controller;
 
+import fr.takima.training.simpleapi.model.Department;
 import fr.takima.training.simpleapi.model.Student;
+import fr.takima.training.simpleapi.repository.DepartmentRepository;
 import fr.takima.training.simpleapi.repository.StudentRepository;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
@@ -10,9 +12,17 @@ import java.util.List;
 public class DepartmentController {
 
   private final StudentRepository studentRepository;
+  private final DepartmentRepository departmentRepository;
 
-  public DepartmentController(StudentRepository studentRepository) {
+  public DepartmentController(StudentRepository studentRepository,
+                              DepartmentRepository departmentRepository) {
     this.studentRepository = studentRepository;
+    this.departmentRepository = departmentRepository;
+  }
+
+  @GetMapping
+  public List<Department> getAllDepartments() {
+    return departmentRepository.findAll();
   }
 
   @GetMapping("/{name}/students")
